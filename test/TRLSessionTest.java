0b84db339abdbac13e90b8d3e768bbbf72439f72
Justin Hui <huixx058@umn.edu>
2017-11-15 16:07:33 -0600

    Changed test cases.

--- test/TRLSessionTest.java
+++ test/TRLSessionTest.java
@@ -1,16 +1,51 @@
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class TRLSessionTest {
 
-	@Before
-	public void setUp() throws Exception {
+	@Test
+	public void testValidatePatron() {
+		fail("Not yet implemented");
 	}
 
 	@Test
-	public void test() {
+	public void testValidateCopy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckOutCopy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWorkerLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPatronID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPatronInfoString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPatronInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCopyInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCanCheckOut() {
 		fail("Not yet implemented");
 	}
 
