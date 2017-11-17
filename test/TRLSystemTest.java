6baef17920f840810ae0c449547228cb4faa7197
Justin Hui <huixx058@umn.edu>
2017-11-17 15:23:43 -0600

    added two JUnit test cases to TRLSystemTest.java

--- test/TRLSystemTest.java
+++ test/TRLSystemTest.java
@@ -5,13 +5,29 @@ import org.junit.Test;
 
 public class TRLSystemTest {
 
+	private Patron patron;
+	private TRLSystem trlSystem;
+	
 	@Before
 	public void setUp() throws Exception {
+		trlSystem = new TRLSystem();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void validatingPatronIdExists() throws Exception {
+		
+		TRLReturnType result=trlSystem.checkPatronID("P1"); //P1 exists in TRLLibrary
+		
+		assertEquals(TRLReturnType.SUCCESS,result);
+	}
+	
+	@Test
+	public void validatingPatronIdNotExist() throws Exception {
+		
+		TRLReturnType result=trlSystem.checkPatronID("P100"); //P100 does not exist in TRLLibrary
+		
+		assertEquals(TRLReturnType.PATRON_NOT_FOUND,result);
 	}
 
+	
 }
