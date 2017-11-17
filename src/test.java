e9d43f36d3d33d9170d11ed34f0dec1cef7008b1
Sheng Lor <Shengylor@gmail.com>
2017-11-17 14:32:47 -0600

    add string to test.java

--- src/test.java
+++ src/test.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 
 public class test {
 // i am testing commiting to Github after creating a new file
+	
+	String test = "just testing";
+
+	public String ReturnTest()
+	{
+		return test;
+	}
+	
 }
+
