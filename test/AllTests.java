7971422a33d497e6fcb95110d095575ed0af557c
Kevin Um <um001069@stthomas.edu>
2017-12-09 11:27:17 -0600

    Updates Session Test to full coverage.

--- test/AllTests.java
+++ test/AllTests.java
@@ -5,6 +5,6 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ CopyTest.class, HoldTest.class, PatronTest.class, TRLLibraryTest.class, TRLSessionTest.class,
 		TRLSystemTest.class })
-public class AllTests {
+	public class AllTests {
 
 }
